dc848c161c64fece476f2cab8176cb4d63a1500c
Ravi <lcs2020043@iiitl.ac.in>
2024-02-03 22:13:09 +0530

    Navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Link to="/products">Products</Link>
+    </>
+  );
 }
